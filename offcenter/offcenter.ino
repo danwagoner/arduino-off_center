db27bbcd48654e35a7e34eea4737823b3f5ac953
Dan Wagoner <danwagoner@users.noreply.github.com>
2024-04-01 13:26:30 -0400

    wip

--- offcenter/offcenter.ino
+++ offcenter/offcenter.ino
@@ -1,54 +1,72 @@
 #include <FastLED.h>
 
-#define LED_PIN     10
-#define NUM_LEDS    300
+#define LED_PIN             10
+#define NUM_LEDS_TOTAL      300
+#define NUM_LEDS            100
+#define NUM_PLATES          5
+#define NUM_LEDS_PER_PLATE  20
+#define MAX_BRIGHTNESS      150
+#define MIN_BRIGHTNESS      10
 
-CRGB leds[NUM_LEDS];
-// CHSV yellow = (45, 200, 255);
+CRGB leds[NUM_LEDS_TOTAL];
+
+struct plate{
+   int direction, speed;
+} plates[NUM_PLATES];
 
 void setup() {
   Serial.begin(9600);
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS_TOTAL);
   FastLED.clear();
-  for (int i = 0; i <= NUM_LEDS; i++) {
-    // leds[i] = CRGB(245, 212, 20); //yellow
-    leds[i] = CHSV (45, 200, 255);
-  }
+  // for (int i = 0; i <= NUM_LEDS; i++) {
+  //   // leds[i] = CRGB(245, 212, 20); //yellow
+  //   leds[i] = CHSV (45, 200, 255);
+  // }
   FastLED.show();
-  // int plate1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  // int plate2[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-  // int plate3[] = {20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
-  // int plate4[] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
-  int plates[4][10] = {
-                        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-                        { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 },
-                        { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 },
-                        { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39 }
-                      };
+
+  for (int p = 0; p < NUM_PLATES; p++){ //init plates
+    plates[p].direction = 1;
+    plates[p].speed = random(1,4);
+    // plates[p].hsv = (45, 200, 255);
+
+    // Serial.print("Plate: ");
+    // Serial.print (p);
+    // Serial.print(" Direction: ");
+    // Serial.print (plates[p].direction);
+    // Serial.print(" Speed: ");
+    // Serial.println (plates[p].speed);
+  }
+
 }
 
 void loop() {
-  int fadespeed = random(1,4);
-  int delayspeed = random(0,10);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    int p = i/NUM_LEDS_PER_PLATE;
+    CHSV _targetHSV = rgb2hsv_approximate(leds[i]);
 
-  CHSV yellow(25, 120, 150);
-  //fade in
-    for (int b = 10; b < 150; b=b+fadespeed) {
-      for (int i = 0; i <= NUM_LEDS; i++) {
-        yellow.v = b;
-        leds[i] = yellow; // update leds[0] with the HSV result
+    if (plates[p].direction == 1) { // fade in
+      _targetHSV.v = _targetHSV.v + plates[p].speed;
+      if ( _targetHSV.v >= 150) {
+        plates[p].direction = 0;
+        // Serial.print("Plate: ");
+        // Serial.print (p);
+        // Serial.print(" Change Direction: ");
+        // Serial.println (plates[p].direction);
       }
-      FastLED.show();
-      delay(delayspeed);
     }
-
-  //fade out
-    for (int b = 150; b >= 10; b=b-fadespeed) {
-      for (int i = 0; i <= NUM_LEDS; i++) {
-        yellow.v = b;
-        leds[i] = yellow; // update leds[0] with the HSV result
+    else { // fade out
+      _targetHSV.v = _targetHSV.v - plates[p].speed;
+      if ( _targetHSV.v <= 0) {
       }
-      FastLED.show();
-      delay(delayspeed);
     }
+    leds[i] = _targetHSV;
+    // Serial.print ("Plate: ");
+    // Serial.print (p);
+    // Serial.print (" Led: ");
+    // Serial.print (i);
+    // Serial.print (" Direction: ");
+    // Serial.print (plates[p].direction);
+    // Serial.print (" Value: ");
+    // Serial.println (_targetHSV.v);
+  }
 }
